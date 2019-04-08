0d23437c670244a3aebee65c6c41c716d59580ff
André Secco <andreluizsecco@yahoo.com.br>
2019-04-08 13:49:15 -0300

    Checking null values when read options from IConfiguration

--- src/XLogger.Adapters.Console/ConsoleLoggerOptions.cs
+++ src/XLogger.Adapters.Console/ConsoleLoggerOptions.cs
@@ -12,9 +12,9 @@ namespace XLogger.Adapters.Console
         public override void ReadFromConfiguration(IConfiguration configuration)
         {
             var consoleConfiguration = configuration.GetSection("XLogger:Console");
-            this.LogLevel = (LogLevel)int.Parse(consoleConfiguration[nameof(LogLevel)]);
-            this.OnDemand = bool.Parse(consoleConfiguration[nameof(OnDemand)]);
-            this.DateTimeFormat = consoleConfiguration[nameof(DateTimeFormat)];
+            this.LogLevel = (LogLevel)int.Parse(consoleConfiguration[nameof(LogLevel)] ?? this.LogLevel.ToString());
+            this.OnDemand = bool.Parse(consoleConfiguration[nameof(OnDemand)] ?? this.OnDemand.ToString());
+            this.DateTimeFormat = consoleConfiguration[nameof(DateTimeFormat) ?? this.DateTimeFormat];
         }
     }
 }
